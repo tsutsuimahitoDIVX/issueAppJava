fa3c2363b610c0ef5bf0b1167982803b05cf1063
tsutsuimahitoDIVX <119909587+tsutsuimahitoDIVX@users.noreply.github.com>
2023-08-23 15:54:49 +0900

    一覧表示機能実装

--- src/main/java/in/techcamp/issueAppJava/IssueController.java
+++ src/main/java/in/techcamp/issueAppJava/IssueController.java
@@ -28,4 +28,11 @@ public class IssueController {
         }
         return "redirect:/";
     }
+
+    @GetMapping
+    public String showIssues(Model model){
+        var issueList = issueRepository.findAll();
+        model.addAttribute("issueList", issueList);
+        return "index";
+    }
 }
